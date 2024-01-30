f28941b2faa24bd70ee4436454a2664c8a81d9ca
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-30 12:11:04 +0900

    feat(react): 선택 기능

--- src/App.js
+++ src/App.js
@@ -11,26 +11,15 @@ export default class App extends Component {
       float: "right"
   }
 
-  getStyle = () => {
+  getStyle = (completed) => {
       return {
           padding: "10px",
           borderBottom: "1px #ccc dotted",
-          textDecoration: "none"
+          textDecoration: completed ? "line-through" : "none"
       }
   }
   state = {
-      todoData : [
-          {
-              id: "1",
-              title: "공부하기",
-              completed: true
-          },
-          {
-              id: "2",
-              title: "청소하기",
-              completed: false
-          }
-      ],
+      todoData : [],
       value: ""
   }
 
@@ -54,8 +43,19 @@ export default class App extends Component {
           completed: false
       }
       //원래 할일 + 새로운 할일
-      this.setState({todoData: [...this.state.todoData, newTodo]})
+      this.setState({todoData: [...this.state.todoData, newTodo], value: ""})
     }
+
+    handleCompleteChange = (id) => {
+      let newTodoData = this.state.todoData.map(data => {
+          if (data.id === id) {
+              data.completed = !data.completed;
+          }
+          return data;
+      })
+        this.setState({todoData : newTodoData});
+    }
+
   render() {
     return(
         <div className="container">
@@ -63,9 +63,10 @@ export default class App extends Component {
                 <div className="title">
                     <h1>할 일 목록</h1>
                 </div>
+
                 {this.state.todoData.map(data => (
-                    <div style={this.getStyle()} key={data.id}>
-                        <input type={"checkbox"} defaultChecked={false}/>
+                    <div style={this.getStyle(data.completed)} key={data.id}>
+                        <input type={"checkbox"} defaultChecked={false} onChange={() => this.handleCompleteChange(data.id)}/>
                         {data.title}
                         <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
                     </div>
