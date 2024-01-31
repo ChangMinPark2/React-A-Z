57791476baf0f7315022f5b2873f2c9facda3759
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-31 16:19:18 +0900

    feat(netflix): axios

---
+++ react-netflix-clone/src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https//api.themoviedb.org/3",
+    params : {
+        api_key: "3ac911c0583248eb7b35dde78c8d6c0d",
+        language: "ko-KR",
+    },
+});
+
+export default instance;
--- react-netflix-clone/src/api/requests.js
+++ react-netflix-clone/src/api/requests.js
@@ -1,11 +1,13 @@
-import axios from "axios";
+const requests = {
+    fetchNowPlaying: "movie/now_playing",
+    fetchNetflixOriginals: "/discover/tv?with_networks=213",
+    fetchTrending: "/trending/all/week",
+    fetchTopRated: "/movie/top_rated",
+    fetchActionMovies: "/discover/movie?with_genres=28",
+    fetchComedyMovies: "/discover/movie?with_genres=35",
+    fetchHorrorMovies: "/discover/movie?with_genres=27",
+    fetchRomanceMovies: "/discover/movie?with_genres=10749",
+    fetchDocumentaries: "/discover/movie?with_genres=99",
+}
 
-const instance = axios.create({
-    baseURL: "https//api.themoviedb.org/3",
-    params : {
-        api_key: "3ac911c0583248eb7b35dde78c8d6c0d",
-        language: "ko-KR",
-    },
-});
-
-export default instance;
+export default requests;
